5f212bf23ce4ec387f02e3e3218b52e8d6fc7ef3
Risto Saarelma <risto.saarelma@iki.fi>
2015-10-25 16:39:01 +0200

    Fix mangled Nock spec

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 //! The Nock spec:
 //!
 //! ```notrust
+//!
 //! A noun is an atom or a cell.
 //! An atom is a natural number.
 //! A cell is an ordered pair of nouns.
@@ -27,23 +28,23 @@
 //! /[(a + a + 1) b] /[3 /[a b]]
 //! /a               /a
 //!
-//! [a [b c] d]     [*[a b c] *[a d]]
+//! *[a [b c] d]     [*[a b c] *[a d]]
 //!
-//! [a 0 b]         /[b a]
-//! [a 1 b]         b
-//! [a 2 b c]       *[*[a b] *[a c]]
-//! [a 3 b]         ?*[a b]
-//! [a 4 b]         +*[a b]
-//! [a 5 b]         =*[a b]
+//! *[a 0 b]         /[b a]
+//! *[a 1 b]         b
+//! *[a 2 b c]       *[*[a b] *[a c]]
+//! *[a 3 b]         ?*[a b]
+//! *[a 4 b]         +*[a b]
+//! *[a 5 b]         =*[a b]
 //!
-//! [a 6 b c d]     *[a 2 [0 1] 2 [1 c d] [1 0] 2 [1 2 3] [1 0] 4 4 b]
-//! [a 7 b c]       *[a 2 b 1 c]
-//! [a 8 b c]       *[a 7 [[7 [0 1] b] 0 1] c]
-//! [a 9 b c]       *[a 7 c 2 [0 1] 0 b]
-//! [a 10 [b c] d]  *[a 8 c 7 [0 3] d]
-//! [a 10 b c]      *[a c]
+//! *[a 6 b c d]     *[a 2 [0 1] 2 [1 c d] [1 0] 2 [1 2 3] [1 0] 4 4 b]
+//! *[a 7 b c]       *[a 2 b 1 c]
+//! *[a 8 b c]       *[a 7 [[7 [0 1] b] 0 1] c]
+//! *[a 9 b c]       *[a 7 c 2 [0 1] 0 b]
+//! *[a 10 [b c] d]  *[a 8 c 7 [0 3] d]
+//! *[a 10 b c]      *[a c]
 //!
-//! a               *a
+//! *a               *a
 //! ```
 
 #![crate_name="nock"]
